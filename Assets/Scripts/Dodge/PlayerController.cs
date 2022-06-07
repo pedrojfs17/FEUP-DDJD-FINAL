ae6466cd8127aa42001433045b8a288eab2f81eb
lipiventura <diogofilipe@MacBook-Pro-de-Diogo.local>
2022-06-07 11:22:49 +0100

    e

--- Assets/Scripts/Dodge/PlayerController.cs
+++ Assets/Scripts/Dodge/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     public float sidewaysForce = 20;
     public float health = 50f;
     public float dir = 1;
+    public string right;
+    public string left;
 
     void Start()
     {
@@ -19,11 +21,11 @@ public class PlayerController : MonoBehaviour
         //rb.AddForce(0,0, forwardForce * Time.deltaTime); 
         rb.AddForce(0,0,dir * 1200 * Time.deltaTime);
 
-        if(Input.GetKey("d")){
+        if(Input.GetKey(right)){
             rb.AddForce(0,0,sidewaysForce * Time.deltaTime);
         }
 
-        if(Input.GetKey("a")){
+        if(Input.GetKey(left)){
             rb.AddForce(0,0,-sidewaysForce * Time.deltaTime);
         }
 
