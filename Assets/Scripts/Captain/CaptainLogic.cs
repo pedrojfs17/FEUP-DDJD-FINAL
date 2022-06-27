53b8db379b875ca22994a6ec5b71e3aa861d65ca
Pedro Seixas <49153615+pedrojfs17@users.noreply.github.com>
2022-06-27 16:45:29 +0100

    Added starting time in captain game

--- Assets/Scripts/Captain/CaptainLogic.cs
+++ Assets/Scripts/Captain/CaptainLogic.cs
@@ -21,11 +21,14 @@ public class CaptainLogic : GameLogic
 
     private List<int> scores;
     private List<TextMeshProUGUI> playerScores;
-    private TextMeshProUGUI accumulator;
+
+    [SerializeField] private TextMeshProUGUI accumulator;
+    [SerializeField] private TextMeshProUGUI countDown;
+    [SerializeField] private TextMeshProUGUI timerText;
+
     private float currentScore = 0;
 
     private TimeSpan gameTimer;
-    public TextMeshProUGUI timerText;
 
     private float Timer = 0;
     private float turnDelay;
@@ -40,8 +43,28 @@ public class CaptainLogic : GameLogic
         turnDelay = 0.4f + numPlayers * 0.1f;
         InitializePlayers();
         UpdateCamera();
-        ResetTimer();
 
+        StartCoroutine(StartCountdown());
+    }
+
+    IEnumerator StartCountdown()
+    {
+        WaitForSeconds waitHalfSecond = new WaitForSeconds(0.5f);
+        WaitForSeconds waitOneSecond = new WaitForSeconds(1);
+
+        yield return waitOneSecond;
+        yield return waitOneSecond;
+
+        for (int i = 3; i > 0; i--) {
+            countDown.text = i.ToString();
+            yield return waitOneSecond;
+        }
+
+        countDown.text = "START";
+        yield return waitHalfSecond;
+        countDown.text = "";
+
+        ResetTimer();
         gameTimer = TimeSpan.FromSeconds(20);
         playing = true;
     }
@@ -55,13 +78,11 @@ public class CaptainLogic : GameLogic
         playerScores = new List<TextMeshProUGUI>();
         scores = new List<int>(new int[numPlayers]);
 
-        accumulator = allScores.GetChild(0).GetComponent<TextMeshProUGUI>();
-
         // Set active only the correct number of players
         for (int i = 0; i < 4; i++) {
             if (i < numPlayers) {
                 players.Add(allPlayers.GetChild(i).gameObject);
-                playerScores.Add(allScores.GetChild(i+1).GetComponent<TextMeshProUGUI>());
+                playerScores.Add(allScores.GetChild(i).GetComponent<TextMeshProUGUI>());
             } else {
                 allPlayers.GetChild(i).gameObject.SetActive(false);
                 allScores.GetChild(i).gameObject.SetActive(false);
@@ -124,8 +145,7 @@ public class CaptainLogic : GameLogic
         Timer -= Time.deltaTime;
 
         if (gameTimer <= TimeSpan.Zero) {
-            playing = false;
-            GameStatus.instance.finishMiniGame(scores);
+            StartCoroutine(finishMiniGame());
             return;
         } else {
             timerText.text = "TIME\n" + gameTimer.ToString("mm':'ss");
@@ -161,11 +181,21 @@ public class CaptainLogic : GameLogic
     
     public override void playerAction(GameObject player)
     {
-        if (!moving && players[currentPlayer] == player) {
+        if (playing && !moving && players[currentPlayer] == player) {
             scores[currentPlayer] += (int) currentScore;
             playerScores[currentPlayer].text = (scores[currentPlayer]).ToString();
             UpdatePlayer();
             UpdateCamera();
         }
+    }
+
+    IEnumerator finishMiniGame()
+    {
+        playing = false;
+        timerText.text = "FINISHED";
+
+        yield return new WaitForSeconds(1.5f);
+
+        GameStatus.instance.finishMiniGame(scores);
     }
 }
