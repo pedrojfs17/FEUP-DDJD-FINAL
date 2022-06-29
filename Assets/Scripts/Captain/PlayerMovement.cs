599ed0a624d893638ab4c2d4c7f7667205e0275a
DanielGSilva <dg.sarmento2000@gmail.com>
2022-06-29 15:37:03 +0100

    Randomized Captain initial order

--- Assets/Scripts/Captain/PlayerMovement.cs
+++ Assets/Scripts/Captain/PlayerMovement.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] public Transform playerInFront;
+    public int playerInBackIndex;
     
     public bool inFront = false;
 
